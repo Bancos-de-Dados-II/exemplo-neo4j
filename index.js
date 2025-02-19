2ff5813ffac25f93bcd0ae662fa5501aabad5de5
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-02-19 14:57:55 -0300

    Atualizando variável da conexão

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ async function conectar(){
     const driver = neo4j.driver(
         process.env.NEO4J_URI,
         neo4j.auth.basic(
-            process.env.NEO4J_USER,
+            process.env.NEO4J_USERNAME,
             process.env.NEO4J_PASSWORD
         )
     );
